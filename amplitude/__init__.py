05994106581180f2f125907f38a9a4e1cfe205e7
Андрей Летюшов <andrei.letyushov@gmail.com>
2020-08-25 13:57:22 +0400

    Add new curl example for API V2 Add more fields in create_event()

--- amplitude/__init__.py
+++ amplitude/__init__.py
@@ -2,14 +2,14 @@ import requests
 import time
 import json
 
-# Documentation of AmplitudeHTTP API:
-#   https://amplitude.zendesk.com/hc/en-us/articles/204771828
+# Documentation of AmplitudeHTTP API V2:
+#   https://developers.amplitude.com/docs/http-api-v2
 #
 # Convert Curl queries - such as below to - python:
 #   https://curl.trillworks.com/
 #
-# Example HTTP Curl Query for Amplitude: 
-#   curl --data 'api_key=SOMEIDOFAKIND' --data 'event=[{"user_id":"john_doe@gmail.com", "event_type":"watch_tutorial", "user_properties":{"Cohort":"Test A"}, "country":"United States", "ip":"127.0.0.1", "time":1396381378123}]' https://api.amplitude.com/httpapi
+# Example HTTP Curl Query for Amplitude:
+# curl --data '{"api_key": "SOMEIDOFAKIND", "events": [{"user_id": "john_doe@gmail.com", "event_type": "watch_tutorial", "user_properties": {"Cohort": "Test A"}, "country": "United States", "ip": "127.0.0.1"}]}' https://api.amplitude.com/2/httpapi
 
 
 class AmplitudeLogger:
@@ -24,37 +24,35 @@ class AmplitudeLogger:
     def turn_off_logging(self):
         self.is_logging = False
 
-    def _is_None_or_not_str(self, value):
-        if value is None or type(value) is not str:
-            return True
-        
     def create_event(self, **kwargs):
-        event = {}
+        event = dict()
         user_id = kwargs.get('user_id', None)
         device_id = kwargs.get('device_id', None)
+        event_type = kwargs.get('event_type', None)
 
-        if self._is_None_or_not_str(user_id) or self._is_None_or_not_str(device_id):
+        if user_id is None or device_id is None or event_type is None:
             return None
-        else:
-            event["device_id"] = device_id
-            event["user_id"] = user_id
+
+        event["device_id"] = device_id if device_id is str else str(device_id)
+        event["user_id"] = user_id if user_id is str else str(user_id)
+        event["event_type"] = event_type if event_type is str else str(event_type)
+        event["time"] = long(time.time() * 1000)
 
         user_properties = kwargs.get('user_properties', None)
         if user_properties is not None and type(user_properties) == dict:
             event["user_properties"] = user_properties
 
-        event_type = kwargs.get('event_type', None)
-        if self._is_None_or_not_str(event_type):
-            return None
-
-        event["event_type"] = event_type
-        
-        # integer epoch time in milliseconds 
-        event["time"] = int(time.time()*1000)
-        
         event_properties = kwargs.get('event_properties', None)
         if event_properties is not None and type(event_properties) == dict:
             event["event_properties"] = event_properties
+
+        app_version = kwargs.get('app_version', None)
+        if app_version is not None and app_version is str:
+            event["app_version"] = app_version
+
+        platform = kwargs.get('platform', None)
+        if platform is not None and platform is str:
+            event["platform"] = platform
 
         event_package = {
             'api_key': self.api_key,
